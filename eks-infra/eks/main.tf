ed7d40c5db2aac30bcdf6b75d7ef2cff7e71461c
Ipeacocks <ipeacocks@protonmail.ch>
2023-06-04 02:16:57 +0300

    Small fixes

--- eks-infra/eks/main.tf
+++ eks-infra/eks/main.tf
@@ -15,26 +15,13 @@ module "eks" {
   version = "19.15.2"
 
   cluster_name    = data.terraform_remote_state.vpc.outputs.eks_cluster_name
-  cluster_version = "1.27"
+  cluster_version = var.cluster_version
 
   vpc_id                         = data.terraform_remote_state.vpc.outputs.vpc_id
   control_plane_subnet_ids       = data.terraform_remote_state.vpc.outputs.control_plane_subnet_ids
   subnet_ids                     = data.terraform_remote_state.vpc.outputs.worker_subnet_ids
   cluster_endpoint_public_access = true
 
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-  }
-
-  eks_managed_node_groups = {
-    one = {
-      name = "node-group-1"
-
-      instance_types = ["t3.small"]
-
-      min_size     = 2
-      max_size     = 3
-      desired_size = 2
-    }
-  }
+  eks_managed_node_group_defaults = var.eks_managed_node_group_defaults
+  eks_managed_node_groups         = var.eks_managed_node_groups
 }
--- eks-infra/eks/variables.tf
+++ eks-infra/eks/variables.tf
@@ -1,3 +1,5 @@
+# Details https://github.com/terraform-aws-modules/terraform-aws-eks#inputs
+
 variable "region" {
   description = "AWS region"
   type        = string
@@ -7,4 +9,36 @@ variable "region" {
 variable "credentials" {
   default     = ["~/.aws/credentials"]
   description = "where your access and secret_key are stored, you create the file when you run the aws config"
+}
+
+variable "cluster_version" {
+  description = "EKS version"
+  type        = number
+  default     = "1.27"
+}
+
+variable "cluster_endpoint_public_access" {
+  description = "Indicates whether or not the Amazon EKS public API server endpoint is enabled"
+  type        = bool
+  default     = true
+}
+
+variable "eks_managed_node_group_defaults" {
+  description = "Map of EKS managed node group default configurations"
+  default     = { ami_type = "AL2_x86_64" }
+}
+
+variable "eks_managed_node_groups" {
+  description = "Map of EKS managed node group definitions to create"
+  default = {
+    one = {
+      name = "node-group-1"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 2
+      max_size     = 3
+      desired_size = 2
+    }
+  }
 }
